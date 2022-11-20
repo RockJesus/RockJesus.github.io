3b620bfec75653c44ea97554ce82dee7934ee1b6
RockJesus <jinyi2318@gmail.com>
2022-11-20 10:20:24 +0800

    Site updated: 2022-11-20 10:20:23

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "10423",
+    "background": "5",
+    "tmpColor": "CCCCCC",
+    "tmpSize": "16",
+    "cityColor": "CCCCCC",
+    "citySize": "16",
+    "aqiColor": "CCCCCC",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "6df613969a4d4095af5d5774b01bb0a4"
+  }
+}
